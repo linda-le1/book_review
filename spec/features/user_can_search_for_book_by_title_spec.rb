faa3a632f50e8a5bfaa2aa757bfad6e3b0536f74
Linda Le <yueling1222@gmail.com>
2020-02-21 09:30:30 -0700

    Add finished spec test

--- spec/features/user_can_search_for_book_by_title_spec.rb
+++ spec/features/user_can_search_for_book_by_title_spec.rb
@@ -4,28 +4,21 @@ describe 'as a user' do
     it 'can find a book and its corresponding info using the search bar' do
         visit '/'
 
-        fill_in :title, with: "the man who saw everything"
+        fill_in :title, with: 'the man who saw everything'
+        click_button 'Find Book'
+
+        expect(current_path).to be eql('/search')
+
+        expect(page).to have_content('Title: The Man Who Saw Everything')
+        expect(page).to have_content('Author: Deborah Levy')
+        expect(page).to have_content('Genre: N/A')
+
+        expect(page).to have_content('Review Count: 2')
+        expect(page).to have_content("The Man Who Saw Everything,” which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at “authoritarian old men.")
+        expect(page).to have_content("Deborah Levy’s latest novel, “The Man Who Saw Everything,” experiments with time travel, history and the endless complications of love.")
+        expect(page).to have_content("Review Publication Date: 2019-10-09")
+        expect(page).to have_content("Review Publication Date: 2019-10-15")
 
     end
 end
 
-
-# As a user
-# When I visit "/"
-# And I input "the man who saw everything" into the form
-# (Note: Use the existing search form)
-# And I click "Find Book"
-# Then I should be on page "/search"
-# Then I should see the book's info
-# For that book I should see
-# - Title
-# - Author
-# - Genres
-# (Note: genres is referred to as "subjects" in the book search response)
-# I should also see:
-# - This book has 2 reviews from the New York Times
-#   (Note: reviews are the "summary" in the book review response)
-# - Review 1: ""The Man Who Saw Everything,"" which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at "authoritarian old men.""
-# Review Publication Date: 2019-10-09
-# - Review 2: "Deborah Levy's latest novel, "The Man Who Saw Everything," experiments with time travel, history and the endless complications of love."
-# Review Publication Date: 2019-10-15
